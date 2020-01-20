2e2bb2d9cfa706f9499d0154d253a78e52106e45
Douglas Walas <douglas.skd@gmail.com>
2020-01-19 21:09:17 -0300

    Projeto criado e rodando um laço de repeticao

--- 01-first-console-app/DigitalInnovationOne/Program.cs
+++ 01-first-console-app/DigitalInnovationOne/Program.cs
@@ -5,8 +5,12 @@ namespace DigitalInnovationOne
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            int NumeroDeVezes = 5;
+            for (int i = 0; i<NumeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
         }
     }
 }
